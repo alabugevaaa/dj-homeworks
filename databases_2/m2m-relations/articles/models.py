286fb75bac7135a289b9e33170421f2822e88587
alabugevaaa <aleksandrachka128@mail.ru>
2020-05-26 21:45:41 +0900

    fix database2

--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 
+class Subject(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Тематика'
+        verbose_name_plural = 'Тематики'
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    subject = models.ManyToManyField(Subject, through='Articleship')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +26,17 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Articleship(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='Статья', related_name='scopes')
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name='Раздел', related_name='scopes')
+    is_main = models.BooleanField(verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
+        ordering = ('-is_main', 'subject__name')
+
+    def __str__(self):
+        return f'{self.article}_{self.subject}'
