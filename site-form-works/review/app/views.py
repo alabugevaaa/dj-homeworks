afd0b288507c7079bddff2978f4a77efeb9e8d6b
alabugevaaa <aleksandrachka128@mail.ru>
2020-05-29 09:32:17 +0900

    fix review

--- site-form-works/review/app/views.py
+++ site-form-works/review/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
+from django.views.generic import FormView
 
 from .models import Product, Review
 from .forms import ReviewForm
@@ -16,18 +17,29 @@ def product_list_view(request):
     return render(request, template, context)
 
 
-def product_view(request, pk):
-    template = 'app/product_detail.html'
-    product = get_object_or_404(Product, id=pk)
+class ProductView(FormView):
 
-    form = ReviewForm
-    if request.method == 'POST':
-        # логика для добавления отзыва
-        pass
+    template_name = 'app/product_detail.html'
+    form_class = ReviewForm
 
-    context = {
-        'form': form,
-        'product': product
-    }
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        product = get_object_or_404(Product, id=kwargs['pk'])
+        reviews = Review.objects.filter(product=product)
+        context['product'] = product
+        context['reviews'] = reviews
+        reviewed_products = request.session.get('reviewed_products') or []
+        is_review_exist = True if kwargs['pk'] in reviewed_products else False
+        context['is_review_exist'] = is_review_exist
+        return self.render_to_response(context)
 
-    return render(request, template, context)
+    def form_valid(self, form):
+        feed = form.save(commit=False)
+        product = get_object_or_404(Product, id=self.kwargs['pk'])
+        feed.product = product
+        feed.save()
+
+        reviewed_products = self.request.session.get('reviewed_products') or []
+        reviewed_products.append(self.kwargs['pk'])
+        self.request.session['reviewed_products'] = reviewed_products
+        return redirect(reverse('main_page'))
